54bcfde9b20e987b95b2965cdc096b27278d621e
marcotristani <marco.tristani@hotmail.it>
2026-02-11 13:07:17 +0100

    refactoring: stampata a console messaggio con metodo,url rotta e tempo nel momento in cui viene chiamata una qualsiasi rotta

--- middlewares/checkTime.js
+++ middlewares/checkTime.js
@@ -2,7 +2,9 @@ function checkTime(req, res, next) {
   //Rintraccio la data corrente da pc
   const timeNow = new Date().toLocaleString();
   //quando viene effettuata una chiamata stampo l'ora e la data
-  console.log("Hai cercato questa rotto il: " + timeNow);
+  console.log(
+    `Hai chiamato la rotta con url " ${req.originalUrl} " con il metodo  " ${req.method} " il : ${timeNow} `,
+  );
   //vado avanti con le richieste
   next();
 }
